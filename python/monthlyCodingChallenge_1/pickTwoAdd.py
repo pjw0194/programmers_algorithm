5e7c2db981c5464749920bb7db34becfe6cb958c
Jinu <pjw019472@gmail.com>
2022-06-24 00:16:50 +0900

    pickTwoAdd problem solve

---
+++ python/monthlyCodingChallenge_1/pickTwoAdd.py
@@ -0,0 +1,30 @@
+# 정수 배열 numbers가 주어집니다. numbers에서 서로 다른 인덱스에 있는 두 개의 수를 뽑아 
+# 더해서 만들 수 있는 모든 수를 배열에 오름차순으로 담아 return 하도록 solution 함수를 완성해주세요.
+
+# numbers의 길이는 2 이상 100 이하입니다.
+# numbers의 모든 수는 0 이상 100 이하입니다.
+
+#  numbers	       result
+# [2,1,3,4,1]	[2,3,4,5,6,7]
+#  [5,0,2,7]	[2,5,7,9,12]
+
+def solution(numbers):
+    answer = []
+    for i in range(len(numbers)):
+        for j in numbers[i + 1:]:
+            sum_result = numbers[i] + j
+            if(not answer.count(sum_result)):
+                answer.append(sum_result)
+    
+    answer.sort()
+        
+    return answer
+
+#  return sorted(list(set(answer))) --> use this to avoid checking if the array has the same sum
+
+
+def main():
+    numbers = [2, 1, 3, 4, 1]
+    print(solution(numbers))
+
+main()
